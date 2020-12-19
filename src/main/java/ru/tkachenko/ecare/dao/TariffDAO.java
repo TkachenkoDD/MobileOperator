015775dadf523eeee7fcfb836471fb5a503eb9c7
TkachenkoDD <tkachenko.dm85@gmail.com>
2020-12-19 19:11:09 +0300

    Data base connection. With JDBC

--- src/main/java/ru/tkachenko/ecare/dao/TariffDAO.java
+++ src/main/java/ru/tkachenko/ecare/dao/TariffDAO.java
@@ -3,41 +3,111 @@ package ru.tkachenko.ecare.dao;
 import org.springframework.stereotype.Component;
 import ru.tkachenko.ecare.models.Tariff;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class TariffDAO {
     private static int TARIFF_COUNT;
-    private List<Tariff> listOfTariffs;
 
-    {
-        listOfTariffs = new ArrayList<>();
-        listOfTariffs.add(new Tariff(++TARIFF_COUNT, "tariff1", 100));
-        listOfTariffs.add(new Tariff(++TARIFF_COUNT, "tariff2", 200));
-        listOfTariffs.add(new Tariff(++TARIFF_COUNT, "tariff3", 300));
+    private static final String URL = "jdbc:postgresql://localhost:5432/ecare";
+    private static final String USERNAME = "tkach";
+    private static final String PASSWORD = "mypassword";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<Tariff> index() {
+        List<Tariff> listOfTariffs = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM tariffs";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                Tariff tariff = new Tariff();
+                tariff.setId(resultSet.getInt("id"));
+                tariff.setTariffName(resultSet.getString("tariffname"));
+                tariff.setTariffCost(resultSet.getInt("tariffcost"));
+                listOfTariffs.add(tariff);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return listOfTariffs;
     }
 
     public Tariff show(int id) {
-        return listOfTariffs.stream().filter(tariff -> tariff.getId() == id).findAny().orElse(null);
+        Tariff tariff = null;
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * FROM tariffs WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            tariff = new Tariff();
+            tariff.setId(resultSet.getInt("id"));
+            tariff.setTariffName(resultSet.getString("tariffname"));
+            tariff.setTariffCost(resultSet.getInt("tariffcost"));
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return tariff;
     }
 
     public void save(Tariff tariff) {
-        tariff.setId(++TARIFF_COUNT);
-        listOfTariffs.add(tariff);
+        try {
+            String SQL = "INSERT INTO tariffs (tariffname, tariffcost)  VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1, tariff.getTariffName());
+            preparedStatement.setInt(2, tariff.getTariffCost());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public void update(int id, Tariff updatedTariff){
-        Tariff tariffToBeUpdated = show(id);
-        tariffToBeUpdated.setTariffName(updatedTariff.getTariffName());
-        tariffToBeUpdated.setTariffCost(updatedTariff.getTariffCost());
+    public void update(int id, Tariff updatedTariff) {
+        try {
+            String SQL = "UPDATE tariffs SET id = ?, tariffname = ?, tariffcost = ? WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setInt(1, updatedTariff.getId());
+            preparedStatement.setString(2, updatedTariff.getTariffName());
+            preparedStatement.setInt(3, updatedTariff.getTariffCost());
+            preparedStatement.setInt(4, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public void delete(int id){
-        listOfTariffs.removeIf(p -> p.getId() == id);
+    public void delete(int id) {
+        try {
+            String SQL = "DELETE FROM tariffs WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
